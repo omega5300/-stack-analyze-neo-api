21d36f88a5c00e4532ea8ab6c27657bbc358e9d6
omega5300 <omegamatrix5351@gmail.com>
2022-10-06 21:59:53 -0500

    update api

--- api/scraping.js
+++ api/scraping.js
@@ -1,12 +1,15 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
+  const { url } = req.query;
+  
   try {
-    const { url } = req.query;
-
-    const { data } = await axios.get(url);
-
-    res.send(data);
+    if(url !== '') {
+      const { data } = await axios.get(url);
+      res.send(data);
+    }
+    
+    res.send('link is required');
   } catch(err) { res.send(err); }
 };
 
