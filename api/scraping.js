20c47ad8480f1e22356fbfee575d6ace138cebe8
omega5300 <omegamatrix5351@gmail.com>
2022-11-05 21:32:02 -0500

    update scraping

--- api/scraping.js
+++ api/scraping.js
@@ -3,13 +3,15 @@ const axios = require('axios');
 module.exports = async (req, res) => {
   const { url } = req.query;
   
+  const webValidation = /https?:\/\//g;
+  
+  if (!webValidation.test(url)) {
+    return res.status(404).send('Please http:// or https:// is required');
+  }
+  
   try {
-    if(url !== '') {
-      const { data } = await axios.get(url);
-      res.send(data);
-    }
-    
-    res.send('link is required');
+    const { data } = await axios.get(url);
+    res.send(data);
   } catch(err) { res.send(err); }
 };
 
